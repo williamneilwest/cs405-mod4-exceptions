517bb16929768f1b4cd3d321f30265159e2006c4
WillWest <williamneilwest@gmail.com>
2023-03-20 11:28:25 -0600

    Initial commit

---
+++ Exceptions.cpp
@@ -0,0 +1,66 @@
+// Exceptions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+bool do_even_more_custom_application_logic()
+{
+    // TODO: Throw any standard exception
+
+    std::cout << "Running Even More Custom Application Logic." << std::endl;
+
+    return true;
+}
+void do_custom_application_logic()
+{
+    // TODO: Wrap the call to do_even_more_custom_application_logic()
+    //  with an exception handler that catches std::exception, displays
+    //  a message and the exception.what(), then continues processing
+    std::cout << "Running Custom Application Logic." << std::endl;
+
+    if(do_even_more_custom_application_logic())
+    {
+        std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+    }
+
+    // TODO: Throw a custom exception derived from std::exception
+    //  and catch it explictly in main
+
+    std::cout << "Leaving Custom Application Logic." << std::endl;
+
+}
+
+float divide(float num, float den)
+{
+    // TODO: Throw an exception to deal with divide by zero errors using
+    //  a standard C++ defined exception
+    return (num / den);
+}
+
+void do_division() noexcept
+{
+    //  TODO: create an exception handler to capture ONLY the exception thrown
+    //  by divide.
+
+    float numerator = 10.0f;
+    float denominator = 0;
+
+    auto result = divide(numerator, denominator);
+    std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+}
+
+int main()
+{
+    std::cout << "Exceptions Tests!" << std::endl;
+
+    // TODO: Create exception handlers that catch (in this order):
+    //  your custom exception
+    //  std::exception
+    //  uncaught exception
+    //  that wraps the whole main function, and displays a message to the console.
+    do_division();
+    do_custom_application_logic();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
