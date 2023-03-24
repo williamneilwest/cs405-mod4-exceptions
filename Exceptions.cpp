aed149cc4c6d65457622538ce2807e12a54476d6
WillWest <williamneilwest@gmail.com>
2023-03-24 13:31:57 -0600

    added exceptions for custom logic and division by zero process

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -5,7 +5,17 @@
 
 bool do_even_more_custom_application_logic()
 {
-    // TODO: Throw any standard exception
+    // Throw any standard exception
+    int x = INT_MAX;
+    int y = 1;
+    int z = x + y;
+    if (z >0){
+        std::cout<<"Addition successful!";
+    }
+    else {
+        throw std::exception();
+    }
+
 
     std::cout << "Running Even More Custom Application Logic." << std::endl;
 
@@ -13,53 +23,83 @@ bool do_even_more_custom_application_logic()
 }
 void do_custom_application_logic()
 {
-    // TODO: Wrap the call to do_even_more_custom_application_logic()
+    // Wrap the call to do_even_more_custom_application_logic()
     //  with an exception handler that catches std::exception, displays
     //  a message and the exception.what(), then continues processing
     std::cout << "Running Custom Application Logic." << std::endl;
+    try{
+        if(do_even_more_custom_application_logic())
+        {
+            std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        }
 
-    if(do_even_more_custom_application_logic())
-    {
-        std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        std::cout << "Leaving Custom Application Logic." << std::endl;
+    } catch (std::exception& e){
+        throw std::overflow_error("Overflow Error!");
+
     }
 
-    // TODO: Throw a custom exception derived from std::exception
-    //  and catch it explictly in main
 
-    std::cout << "Leaving Custom Application Logic." << std::endl;
 
 }
 
 float divide(float num, float den)
 {
-    // TODO: Throw an exception to deal with divide by zero errors using
+    // Throw an exception to deal with divide by zero errors using
     //  a standard C++ defined exception
-    return (num / den);
+
+    if (den == 0){
+        throw std::runtime_error("Division by zero!");
+    }
+
+
 }
 
-void do_division() noexcept
+void do_division()
 {
-    //  TODO: create an exception handler to capture ONLY the exception thrown
+    //  create an exception handler to capture ONLY the exception thrown
     //  by divide.
 
-    float numerator = 10.0f;
-    float denominator = 0;
+     try {
+         float numerator = 10.0f;
+         float denominator = 0;
+         auto result = divide(numerator, denominator);
+         std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+     }catch (std::runtime_error& e){
+         throw std::runtime_error("Division by 0 is undefined!");
+         }
 
-    auto result = divide(numerator, denominator);
-    std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
 }
 
-int main()
-{
-    std::cout << "Exceptions Tests!" << std::endl;
+int main() {
+    try {
+        std::cout << "Exceptions Tests!" << std::endl;
 
-    // TODO: Create exception handlers that catch (in this order):
-    //  your custom exception
-    //  std::exception
-    //  uncaught exception
-    //  that wraps the whole main function, and displays a message to the console.
-    do_division();
-    do_custom_application_logic();
+        // Create exception handlers that catch (in this order):
+        //  your custom exception
+        //  std::exception
+        //  uncaught exception
+        //  that wraps the whole main function, and displays a message to the console.
+
+        try {
+            do_division();
+        }
+        catch (std::runtime_error &e) {
+            std::cout << e.what() << std::endl;
+        }
+        try {
+            do_custom_application_logic();
+        }
+        catch(std::overflow_error& e){
+            std::cout<<e.what()<<std::endl;
+
+        }
+
+
+    }
+    catch(std::exception& f){
+        std::cout<<f.what()<<std::endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
