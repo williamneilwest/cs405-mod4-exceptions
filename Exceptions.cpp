50f8f2b30ef8aef836861d8175b5c5fc137728ee
WillWest <williamneilwest@gmail.com>
2023-04-01 11:02:18 -0600

    fixed exception logic

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -15,8 +15,6 @@ bool do_even_more_custom_application_logic()
     else {
         throw std::exception();
     }
-
-
     std::cout << "Running Even More Custom Application Logic." << std::endl;
 
     return true;
@@ -32,10 +30,9 @@ void do_custom_application_logic()
         {
             std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
         }
-
         std::cout << "Leaving Custom Application Logic." << std::endl;
-    } catch (std::exception& e){
-        throw std::overflow_error("Overflow Error!");
+    } catch (std::exception& f){
+        std::cout<<"Overflow Error! "<<f.what()<<std::endl;
 
     }
 
@@ -49,8 +46,9 @@ float divide(float num, float den)
     //  a standard C++ defined exception
 
     if (den == 0){
-        throw std::runtime_error("Division by zero!");
+        throw std::exception();
     }
+    return num/den;
 
 
 }
@@ -61,45 +59,41 @@ void do_division()
     //  by divide.
 
      try {
-         float numerator = 10.0f;
-         float denominator = 0;
-         auto result = divide(numerator, denominator);
-         std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
-     }catch (std::runtime_error& e){
-         throw std::runtime_error("Division by 0 is undefined!");
+         float num;
+         float den;
+         std::cout<<"Numerator: ";
+         std::cin>>num;
+         std::cout<<"Denominator: ";
+         std::cin>>den;
+         auto result = divide(num, den);
+         std::cout << "divide(" << num << ", " << den << ") = " << result << std::endl;
+     }catch (const std::exception& e){
+         std::cout<<"Division by 0 is undefined!"<<std::endl;
          }
 
 }
 
 int main() {
-    try {
-        std::cout << "Exceptions Tests!" << std::endl;
+    std::cout << "Exceptions Tests!" << std::endl;
 
+    try {
         // Create exception handlers that catch (in this order):
         //  your custom exception
         //  std::exception
         //  uncaught exception
         //  that wraps the whole main function, and displays a message to the console.
-
-        try {
-            do_division();
-        }
-        catch (std::runtime_error &e) {
-            std::cout << e.what() << std::endl;
-        }
-        try {
-            do_custom_application_logic();
-        }
-        catch(std::overflow_error& e){
-            std::cout<<e.what()<<std::endl;
-
-        }
-
-
+        do_division();
+        do_custom_application_logic();
     }
-    catch(std::exception& f){
-        std::cout<<f.what()<<std::endl;
+    catch (std::runtime_error& f) {
+        std::cout << f.what() << std::endl;
     }
+    catch(std::exception& e){
+        std::cout<<e.what()<<std::endl;
+    }
+    catch(...){
+    }
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
